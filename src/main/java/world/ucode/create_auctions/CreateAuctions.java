d9b3d01ec32ec17d73b0ab62b0ea9ce2c45ace7b
vstruk01 <stoliar444@gmail.com>
2020-11-04 21:18:47 +0200

    add create_auctions page

---
+++ src/main/java/world/ucode/create_auctions/CreateAuctions.java
@@ -0,0 +1,16 @@
+package world.ucode.create_auctions;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/create_auctions")
+public class CreateAuctions extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/jsp/create_auctions.jsp").forward(req, resp);
+    }
+}
---
+++ src/main/webapp/jsp/create_auctions.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: vstruk
+  Date: 11/4/20
+  Time: 9:06 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <Title>ubay - create auctions</title>
+</head>
+<body>
+    <h1>Hello World!</h1>
+</body>
+</html>
