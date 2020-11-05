64e174b778575b91c35d36c80afe88489ab47e6a
vstruk01 <stoliar444@gmail.com>
2020-11-05 14:09:35 +0200

    work with request

--- src/main/java/world/ucode/create_auctions/CreateAuctions.java
+++ src/main/java/world/ucode/create_auctions/CreateAuctions.java
@@ -1,11 +1,16 @@
 package world.ucode.create_auctions;
 
+import jdk.incubator.jpackage.internal.IOUtils;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 @WebServlet("/create_auctions")
 public class CreateAuctions extends HttpServlet {
@@ -16,5 +21,16 @@ public class CreateAuctions extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("hello post request :)");
+        resp.setStatus(201);
+        resp.setContentType("text/plain");
+        String s = "Hello World!";
+        InputStream is =  new ByteArrayInputStream(s.getBytes());
+        OutputStream os = resp.getOutputStream();
+        byte[] buff = new byte[1024];
+        int sizeBuff = 0;
+
+        while ((sizeBuff = is.read(buff)) != -1) {
+            os.write(buff, 0, sizeBuff);
+        }
     }
 }
--- src/main/webapp/js/create_auctions/create_auctions.js
+++ src/main/webapp/js/create_auctions/create_auctions.js
@@ -1,13 +1,30 @@
 console.log("Hello World!");
 
-document.getElementById("sub").addEventListener('click', function () {
+document.getElementById('sub').addEventListener('click',  async function () {
     console.log('tl: ' + document.getElementById('title_auction').value);
     console.log('dc: ' + document.getElementById('description').value);
     console.log('sp: ' + document.getElementById('start_price').value);
     console.log('mp: ' + document.getElementById('max_price').value);
     console.log('st: ' + document.getElementById('start_time').value);
     console.log('dr: ' + document.getElementById('duration').value);
+
+    let formData = new FormData();
     for (let i = 0; document.getElementById('forImage').files[i] != null; i++ ) {
-        console.log('im: ' + document.getElementById('forImage').files[i].name);
+        formData.append(document.getElementById('forImage').files[i].name, document.getElementById('forImage').files[i])
+    }
+
+    let response = await fetch("http://localhost:8080/ubay/create_auctions", {
+        method: 'POST',
+        // enctype: 'mutlipart/form-data'
+        headers: {
+            'Content-Type': 'multipart/form-data'
+        },
+        body: formData
+    })
+    if (response.ok) {
+        let text = await response.text();
+        console.log('ok: ' + text);
+    } else {
+        console.log('error');
     }
 })
