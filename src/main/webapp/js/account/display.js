52b5368ce6f1e21cd45a7c6c169f15a2524826f1
Yurii Makohon <yuriimakohon@gmail.com>
2020-11-08 14:45:09 +0200

    added some content to "/account" url

---
+++ src/main/webapp/js/account/display.js
@@ -0,0 +1,22 @@
+let changePasswordContainer = $('#change_pass-container');
+let loginItem = $('#login-item');
+let btnChangePass = $('#btn-change_pass');
+let infoText = $('#info_text');
+
+function onChangePassword() {
+    loginItem.addClass('hidden');
+    btnChangePass.addClass('hidden');
+    changePasswordContainer.removeClass('hidden');
+}
+
+function onCancelPass() {
+    loginItem.removeClass('hidden');
+    btnChangePass.removeClass('hidden');
+    changePasswordContainer.addClass('hidden');
+}
+
+function onSavePass() {
+    infoText.text('Password was successfully changed');
+    infoText.addClass('success_text');
+    infoText.removeClass('hidden');
+}
