ac7373bb41c8d2ca0d845126d4bcb403845097b9
Yurii Makohon <yuriimakohon@gmail.com>
2020-11-08 21:39:17 +0200

    redirect from root to Catalog

---
+++ src/main/java/world/ucode/API/catalog/Catalog.java
@@ -0,0 +1,20 @@
+package world.ucode.API.catalog;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/catalog")
+public class Catalog extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ServletContext servletContext = getServletContext();
+        RequestDispatcher requestDispatcher = servletContext.getRequestDispatcher("/jsp/catalog.jsp");
+        requestDispatcher.forward(req, resp);
+    }
+}
