42f09472ff25423cd520b8cdc9d563f3ee062524
Yurii Makohon <yuriimakohon@gmail.com>
2020-11-10 20:51:59 +0200

    generate lots in catalog

--- src/main/webapp/js/catalog/generate_lots.js
+++ src/main/webapp/js/catalog/generate_lots.js
@@ -1,7 +1,25 @@
+function lotHtmlGen(lotId, title, price) {
+    return ['                    <div class="card auctions-item" onclick="onAuction(' + lotId + ')">\n' +
+    '                        <img src="">\n' +
+    '                        <div class="text">\n' +
+    '                            <div class="title">\n' +
+    '                                <p>' + title + '</p>\n' +
+    '                            </div>\n' +
+    '                            <p class="price">' + price + '</p>\n' +
+    '                        </div>\n' +
+    '                    </div>\n'];
+}
+
 function lotsGen(json) {
+    let container = $('#auctions-container');
+
     json.lots.forEach(el => {
         let parsed = JSON.parse(el);
-        console.log(parsed['title'] + "\n" + parsed['description']);
+        let title = parsed['title'];
+        let price = parsed['price'];
+        let lotId = parsed['lotId'];
+        let lot = lotHtmlGen(lotId, title, price);
+
+        container.append(lot);
     });
-    console.log(json.lots[0]);
 }
