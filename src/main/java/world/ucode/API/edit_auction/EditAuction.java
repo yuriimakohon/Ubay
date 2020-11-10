3792055c5bc794c7934c441348415fb10d98220e
Yurii Makohon <yuriimakohon@gmail.com>
2020-11-10 19:42:36 +0200

    created "edit auction branch"

--- src/main/java/world/ucode/API/edit_auction/EditAuction.java
+++ src/main/java/world/ucode/API/edit_auction/EditAuction.java
@@ -1,4 +1,4 @@
-package world.ucode.API.catalog;
+package world.ucode.API.edit_auction;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -7,10 +7,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/catalog")
-public class Catalog extends HttpServlet {
+@WebServlet("/edit_auction")
+public class EditAuction extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher("/jsp/catalog.jsp").forward(req, resp);
+        req.getRequestDispatcher("/jsp/edit_auction.jsp").forward(req, resp);
     }
 }
