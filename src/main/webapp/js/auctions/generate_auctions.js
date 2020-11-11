fd6df42017860abcf5a156ce86421ded4df14ac8
Yurii Makohon <yuriimakohon@gmail.com>
2020-11-11 12:59:53 +0200

    separated queries for 'Edit btn' click and 'Lot' click

--- src/main/webapp/js/auctions/generate_auctions.js
+++ src/main/webapp/js/auctions/generate_auctions.js
@@ -1,12 +1,12 @@
 function auctionHtmlGen(lotId, title, price, bidsCount) {
     return ['        <div class="auction-item card">\n' +
-    '            <img src="resources/test.png" alt="auction-' + lotId + '">\n' +
+    '            <img src="resources/test.png" alt="auction-' + lotId + '" onclick="onAuction(' + lotId + ')">\n' +
     '            <div class="info_block">\n' +
-    '                <div class="title">' + title + '</div>\n' +
+    '                <div class="title" onclick="onAuction(' + lotId + ')">' + title + '</div>\n' +
     '                <div class="price">' + price + '</div>\n' +
     '            </div>\n' +
     '            <div class="control_block">\n' +
-    '                <button class="btn">edit</button>\n' +
+    '                <button class="btn" onclick="onAuctionEdit(' + lotId + ')">edit</button>\n' +
     '                <div class="bids_label">Bids:</div>\n' +
     '                <div class="bids_count">' + bidsCount + '</div>\n' +
     '            </div>\n' +
@@ -31,3 +31,5 @@ function auctionsGen(json) {
     auction = auctionHtmlGen(2, 'Coin with Woolf (America 2005)', 12.40, 23);
     container.append(auction);
 }
+
+auctionsGen();
