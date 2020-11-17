680527ab4152af2b9031bdd6ae6dfebacddfbbca
ostanislav <Zeemer.1997@gmail.com>
2020-11-17 23:03:20 +0200

    repair db table

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -7,7 +7,10 @@ DROP TABLE IF EXISTS users;
 
 
 CREATE TABLE IF NOT EXISTS users (id INTEGER NOT NULL AUTO_INCREMENT, username VARCHAR(255), login VARCHAR(255), password VARCHAR(255), token TEXT, ref_token TEXT, balance DOUBLE, userRole integer, userphoto TEXT, PRIMARY KEY (id));
-
+INSERT into users VALUES (123, "admin1", "first", "lol", "token1", "ref token1",0, 0, "userphoto");
+INSERT into users VALUES (1234, "admin2", "second", "lol", "token2", "ref token1", 0, 0, "userphoto");
+INSERT into users VALUES (12345, "admin3", "third", "lol", "token3", "ref token1",1, 0, "userphoto");
+INSERT into users VALUES (123456, "admin4", "four", "lol", "token4", "ref token1", 1, 0, "userphoto");
 
 CREATE TABLE IF NOT EXISTS lot (id INTEGER primary key NOT NULL AUTO_INCREMENT, sellerId INTEGER, title VARCHAR(255), price DOUBLE, photo VARCHAR(255), maxPrice DOUBLE, startTime BIGINT, duration BIGINT, description TEXT, status integer, highestBid DOUBLE, bidderid integer,  bidNumber integer, photoNumber integer, category TEXT, rate integer, FOREIGN KEY (sellerId) REFERENCES users(id) ON DELETE CASCADE);
 INSERT into lot VALUES (1, 123, "tittle1", 1, "photo1", 1, 1, 1, "description1", 1, 5, 12345,0, 1,"Sport",2);
