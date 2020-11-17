f8243781d2d6f5decce2c77d2683993ebbd3a9b4
vstruk01 <stoliar444@gmail.com>
2020-11-17 16:48:29 +0200

    update desc to test update balance from bigint to double

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -6,9 +6,9 @@ DROP TABLE IF EXISTS users;
 
 
 
-CREATE TABLE IF NOT EXISTS users (id INTEGER NOT NULL AUTO_INCREMENT, username VARCHAR(255), login VARCHAR(255), password VARCHAR(255), token TEXT, ref_token TEXT, balance BIGINT, userRole integer, userphoto TEXT, PRIMARY KEY (id));
+CREATE TABLE IF NOT EXISTS users (id INTEGER NOT NULL AUTO_INCREMENT, username VARCHAR(255), login VARCHAR(255), password VARCHAR(255), token TEXT, ref_token TEXT, balance DOUBLE, userRole integer, userphoto TEXT, PRIMARY KEY (id));
 
-CREATE TABLE IF NOT EXISTS lot (id INTEGER primary key NOT NULL AUTO_INCREMENT, sellerId INTEGER, title VARCHAR(255), price BIGINT, photo VARCHAR(255), maxPrice BIGINT, startTime BIGINT, duration BIGINT, description VARCHAR(255), status integer, highestBid integer, bidderid integer,  bidNumber integer, photoNumber integer, category TEXT, FOREIGN KEY (sellerId) REFERENCES users(id) ON DELETE CASCADE);
+CREATE TABLE IF NOT EXISTS lot (id INTEGER primary key NOT NULL AUTO_INCREMENT, sellerId INTEGER, title VARCHAR(255), price BIGINT, photo VARCHAR(255), maxPrice BIGINT, startTime BIGINT, duration BIGINT, description TEXT, status integer, highestBid integer, bidderid integer,  bidNumber integer, photoNumber integer, category TEXT, FOREIGN KEY (sellerId) REFERENCES users(id) ON DELETE CASCADE);
 INSERT into lot VALUES (1, 123, "tittle1", 1, "photo1", 1, 1, 1, "description1", 0, 5, 12345,0, "Sport");
 INSERT into lot VALUES (2, 123, "tittle2", 1, "photo1", 1, 1, 1, "description1", 0, 5, 12345, 0, "Sport;Techika;Goods");
 INSERT into lot VALUES (3, 1234, "tittle3", 1, "photo1", 1, 1, 1, "description1", 0, 5, 12346,0, "Sport;Goods;Techika");
