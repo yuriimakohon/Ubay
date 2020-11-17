d4edea60a30f7d153fc4aedd2aad7bd07c1e5ffe
vstruk01 <stoliar444@gmail.com>
2020-11-17 21:31:11 +0200

    change price bigint to double

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,20 +1,20 @@
--- DROP TABLE IF EXISTS feedback;
--- DROP TABLE IF EXISTS bid;
--- DROP TABLE IF EXISTS lot;
--- DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS feedback;
+DROP TABLE IF EXISTS bid;
+DROP TABLE IF EXISTS lot;
+DROP TABLE IF EXISTS users;
 
 
 
 
 CREATE TABLE IF NOT EXISTS users (id INTEGER NOT NULL AUTO_INCREMENT, username VARCHAR(255), login VARCHAR(255), password VARCHAR(255), token TEXT, ref_token TEXT, balance DOUBLE, userRole integer, userphoto TEXT, PRIMARY KEY (id));
 
-CREATE TABLE IF NOT EXISTS lot (id INTEGER primary key NOT NULL AUTO_INCREMENT, sellerId INTEGER, title VARCHAR(255), price DOUBLE , photo VARCHAR(255), maxPrice DOUBLE , startTime BIGINT, duration BIGINT, description TEXT, status integer, DOUBLE integer, bidderid integer,  bidNumber integer, photoNumber integer, category TEXT, FOREIGN KEY (sellerId) REFERENCES users(id) ON DELETE CASCADE);
+CREATE TABLE IF NOT EXISTS lot (id INTEGER primary key NOT NULL AUTO_INCREMENT, sellerId INTEGER, title VARCHAR(255), price DOUBLE, photo VARCHAR(255), maxPrice DOUBLE, startTime BIGINT, duration BIGINT, description TEXT, status integer, highestBid DOUBLE, bidderid integer,  bidNumber integer, photoNumber integer, category TEXT, FOREIGN KEY (sellerId) REFERENCES users(id) ON DELETE CASCADE);
 -- INSERT into lot VALUES (1, 123, "tittle1", 1, "photo1", 1, 1, 1, "description1", 0, 5, 12345,0, "Sport");
 -- INSERT into lot VALUES (2, 123, "tittle2", 1, "photo1", 1, 1, 1, "description1", 0, 5, 12345, 0, "Sport;Techika;Goods");
 -- INSERT into lot VALUES (3, 1234, "tittle3", 1, "photo1", 1, 1, 1, "description1", 0, 5, 12346,0, "Sport;Goods;Techika");
 -- INSERT into lot VALUES (4, 1234, "tittle4", 1, "photo1", 1, 1, 1, "description1", 0, 5, 12346, 0, "Goods;Techika;Color");
 
-CREATE TABLE IF NOT EXISTS bid (id INTEGER primary key NOT NULL AUTO_INCREMENT, lotid INTEGER, bidderId INTEGER, price DOUBLE , statusOfBid INTEGER, FOREIGN KEY (bidderId) REFERENCES users(id) ON DELETE CASCADE, FOREIGN KEY (lotid) REFERENCES lot(id) ON DELETE CASCADE);
+CREATE TABLE IF NOT EXISTS bid (id INTEGER primary key NOT NULL AUTO_INCREMENT,lotid INTEGER, bidderId INTEGER, price DOUBLE, statusOfBid INTEGER, FOREIGN KEY (bidderId) REFERENCES users(id) ON DELETE CASCADE, FOREIGN KEY (lotid) REFERENCES lot(id) ON DELETE CASCADE);
 -- INSERT into bid VALUES (5, 3, 12345, 3, 1);
 -- INSERT into bid VALUES (1,4, 12345, 2, 1);
 -- INSERT into bid VALUES (2,3, 123456, 1, 1);
