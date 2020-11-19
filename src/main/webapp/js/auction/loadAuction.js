9db9c9be63d7a5405bf063dfe91fc4900d371869
vstruk01 <stoliar444@gmail.com>
2020-11-19 18:33:17 +0200

    add loading categories

--- src/main/webapp/js/auction/loadAuction.js
+++ src/main/webapp/js/auction/loadAuction.js
@@ -13,5 +13,28 @@ document.addEventListener("DOMContentLoaded", async function () {
         // $('#input-start_time').val(lot.startTime);
         rd.val((+lot.duration - +lot.startTime) / 24 / 3600 / 1000);
         $('#p-duration').text(rd.val());
+
+        let collectibles = $('#checkbox-collectibles');
+        let technics = $('#checkbox-technics');
+        let furniture = $('#checkbox-furniture');
+        let clothing = $('#checkbox-clothing');
+        let household = $('#checkbox-household');
+        let jewellery = $('#checkbox-jewellery');
+
+        if (lot.category.indexOf('collectibles') !== -1)
+            collectibles.prop('checked', true);
+        if (lot.category.indexOf('technics') !== -1)
+            technics.prop('checked', true);
+        if (lot.category.indexOf('furniture') !== -1)
+            furniture.prop('checked', true);
+        if (lot.category.indexOf('clothing') !== -1)
+            clothing.prop('checked', true);
+        if (lot.category.indexOf('household') !== -1)
+            household.prop('checked', true);
+        if (lot.category.indexOf('jewellery') !== -1)
+            jewellery.prop('checked', true);
+
     }
 })
+
+
