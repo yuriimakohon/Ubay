dd09ba90fff1c2dc6df5cfa7b9ebffa371f31f8f
vstruk01 <stoliar444@gmail.com>
2020-11-20 16:37:57 +0200

    refactor servlet and request token

--- src/main/webapp/js/auctions/getMyAuctions.js
+++ src/main/webapp/js/auctions/getMyAuctions.js
@@ -1,8 +1,4 @@
 document.addEventListener("DOMContentLoaded", async function loadMyAuctions() {
-    if (await ref_token()) {
-        window.location.replace('/authorization');
-    }
-
     let response = await fetch('http://localhost:8080/api/search?user_id=' + localStorage.getItem('id'), {
         method: 'GET',
         credentials: "same-origin"
@@ -14,3 +10,5 @@ document.addEventListener("DOMContentLoaded", async function loadMyAuctions() {
         console.log("what is wrong ?");
     }
 });
+
+
