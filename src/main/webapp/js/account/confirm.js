0ed7c721895d1ef2b1f789c79ff62ce60f245de8
Yurii Makohon <yuriimakohon@gmail.com>
2020-11-22 17:11:04 +0200

    fixed: success info text changed to error

--- src/main/webapp/js/account/confirm.js
+++ src/main/webapp/js/account/confirm.js
@@ -29,7 +29,7 @@ async function onSaveLogin() {
         if (await changeLogin(inputLogin.val())) {
             successNewLogin();
         } else {
-            loginExists();
+            errLoginExists();
         }
     }
 }
